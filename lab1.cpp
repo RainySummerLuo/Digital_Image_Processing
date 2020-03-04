e8218d19b33fa52f9bb12d8ed884bdca93124413
Laurence Luo <iluo@163.com>
2020-03-04 20:51:30 +0800

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -9,8 +9,7 @@ using namespace cv;
 int lab1_2_1(char *filename) {
     printf("OpenCV: Hello, world!\n");
     Mat image = imread(filename);
-    if (image.empty())
-    {
+    if (image.empty()) {
         std::cout << "Hey! Can't read the image!" << std::endl;
         system("PAUSE");
         return 1;
@@ -23,8 +22,9 @@ int lab1_2_1(char *filename) {
 
 int lab1_2_2(char *filename) {
     Mat srcImage = cv::imread(filename);
-    if (!srcImage.data)
+    if (!srcImage.data) {
         return 1;
+    }
     Mat greyImage;
     cvtColor(srcImage, greyImage, COLOR_BGR2GRAY);
     namedWindow("Lena Image - Grey 1");
@@ -34,10 +34,11 @@ int lab1_2_2(char *filename) {
 }
 
 int lab1_2_3(char *filename) {
-    IplImage* srcImg;
+    IplImage *srcImg;
     srcImg = cvvLoadImage(filename);
-    if (!srcImg)
+    if (!srcImg) {
         return 1;
+    }
     IplImage *greyImage = cvCreateImage(cvGetSize(srcImg), srcImg->depth, srcImg->alphaChannel);
     cvCvtColor(srcImg, greyImage, CV_BGR2GRAY);
     cvvShowImage("Lena Image - Grey 2", greyImage);
